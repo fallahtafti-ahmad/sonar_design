680520bb2804cef4edcf56e29b60587d7b56ef9d
fallahtafti-ahmad <fallahtaftiahmad@gmail.com>
2023-11-11 17:51:48 +0330

    update

--- tests/test_3d_drawing.py
+++ tests/test_3d_drawing.py
@@ -14,8 +14,8 @@ for z in np.linspace(hs,he,100):
         vertices.append([x,y,z])
         faces.append([0,1,2])
 print(len(vertices))
-mesh=tm.creation.capsule(height=5,count=[1,1])
-# mesh = tm.Trimesh(vertices=[[0, 0, 0], [0, 0, 1], [0, 1, 0],[1,0,0]],
-                    #    faces=[[0, 1, 2],[0, 1, 3],[0, 2, 3],[1,2,3]])
+# mesh=tm.creation.capsule(height=5)
+mesh = tm.Trimesh(vertices=[[0, 0, 0], [0, 0, 1], [0, 1, 0],[1,0,0]],
+                       faces=[[0, 1, 2],[0, 1, 3],[0, 2, 3],[1,3,2]])
 # mesh = tm.Trimesh(vertices=vertices,faces=faces)
 mesh.show()
