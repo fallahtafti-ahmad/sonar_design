e7ecd30fba936db94ef5c02d0f10efbafb09b46c
fallahtafti-ahmad <fallahtaftiahmad@gmail.com>
2023-12-10 17:01:50 +0330

    Update test2.py

--- tests/test2.py
+++ tests/test2.py
@@ -1,4 +1,15 @@
-for i in range(500):
-    print("hello"+str(i))
-input("press any key")
-    
+import trimesh
+from PIL import Image
+import io
+tm = trimesh.load("assets\Typhoon.stl")
+sc = trimesh.Scene()
+
+# sc.add_geometry(tm)
+slice = tm.section(plane_origin=tm.centroid,
+                     plane_normal=[0,0,1])
+
+sc.add_geometry(slice)
+png=sc.save_image(resolution=(800,600))
+sc.show(resolution=(800,600))
+image=Image.open(io.BytesIO(png))
+image.show()
